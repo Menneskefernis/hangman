3bb5f608306c8a7d1617b4db01ddfb9a3679406f
Henrik Nørgaard <menneskefernis@gmail.com>
2019-12-20 23:06:16 +0100

    working version of all tasks

--- hangman.rb
+++ hangman.rb
@@ -1,19 +1,26 @@
 require 'colorize'
+require 'msgpack'
 
 class Game
-  attr_accessor :words, :current_word, :guess_state, :attempts_left
+  attr_accessor :words, :current_word, :guess_state, :attempts_left, :end_game
   
   def initialize
-    @words = ["paper", "winter", "bicycle", "truck", "garbage", "chess"]
+    @words = dictionary
     @current_word = random_word
     @guess_state = initialize_guess_string
     @attempts_left = 4
+    @end_game = false
   end
 
-  def start
-    #puts current_word
+  def start    
+    puts "Welcome to a game of Hangman!"
+    puts "You can save your progress any time by typing 'save'."
+    puts ""
+    request_old_game
+ 
+    puts current_word
 
-    while guess_state.include?("_") && attempts_left > 0
+    until end_game || attempts_left < 1
       play_round
     end
   end
@@ -24,7 +31,69 @@ class Game
     puts ""
     show_guess_state
     input = get_input
-    check_guess(input)
+    if input == "save"
+      handle_save
+    else
+      check_guess(input)
+    end
+  end
+
+  def handle_save
+    puts "What name would you like to use for your save file?"
+    
+    input = gets.chomp
+    Dir.mkdir('./save_files') unless Dir.exist?('./save_files')
+    to_msgpack(input)
+    self.end_game = true
+  end
+
+  def request_old_game
+    puts "Would you like to load an old game? (y/n)".green
+    
+    input = ""
+    loop do
+      input = gets.chomp.downcase
+      break if input == "y" || input == "n"
+      puts "Enter 'y' or 'n'."
+    end
+    
+    input == "y" ? handle_game_load : return
+  end
+
+  def handle_game_load
+    puts "What game would you like to load?"
+    puts ""
+
+    files = Dir["./save_files/*.dat"]
+    list_savegames(files)
+
+    input = ""
+    loop do
+      input = gets.chomp.to_i
+      break if (1 .. files.size).include?(input)
+      puts "Enter a file number."
+    end
+
+    from_msgpack(files[input - 1])
+    return
+  end
+
+  def list_savegames(files)
+    i = 1
+    
+    files.each do |f|
+      filename = f.split('/').last
+      puts i.to_s + ": " + filename
+      i += 1
+    end
+  end
+
+  def pick_game
+
+  end
+
+  def dictionary
+    File.readlines('dictionary.txt')
   end
 
   def initialize_guess_string
@@ -59,13 +128,15 @@ class Game
   end
 
   def win
+    self.end_game = true
     puts ""
     puts "You guessed the right word: " + current_word.upcase.green
   end
 
   def lose
     puts ""
-    puts "You are out of attempts. The man was hung!".red
+    puts "You are out of attempts. The word was #{current_word.upcase}!".red
+    puts "The man was hung!"
     puts ""
     puts " |     |"
     puts " |     |"
@@ -78,7 +149,7 @@ class Game
   end
 
   def random_word
-    words.sample
+    words.select { |word| word.size > 5 && word.size <= 12 }.sample.strip.downcase
   end
 
   def get_input    
@@ -87,14 +158,33 @@ class Game
     loop do
       input = gets.chomp.downcase
 
-      if input =~ /[A-Za-z]/ && input.size == 1
-        return input
-      else
-        puts "Enter ONE letter".red
-      end
+      return input if (input =~ /[A-Za-z]/ && input.size == 1) || input == "save"
+      puts "Enter ONE letter"
+      
     end
   end
 
+  def to_msgpack(filename)
+    data = {
+      current_word: current_word.strip,
+      guess_state: guess_state.strip,
+      attempts_left: attempts_left
+    }.to_msgpack
+
+    File.open("./save_files/#{filename}.dat", 'w') { |file| file.write data}
+  end
+
+  def from_msgpack(file)
+    data = File.read(file)
+    
+    obj = MessagePack.unpack(data)
+    
+    self.current_word = obj['current_word']
+    self.guess_state = obj['guess_state']
+    self.attempts_left = obj['attempts_left']
+    
+    puts "Your data has been loaded."
+  end
 end
 
 hangman = Game.new
